75a08a6614acba844b6938933a0dbcb87bbeaf39
Oleksandr Pustovit <oleksandr.pustovit@fieldbee.com>
2026-05-27 17:34:44 +0300

    Version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.km.warehouse"
         minSdk = 28
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.1.2"
+        versionCode = 20
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
